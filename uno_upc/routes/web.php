47391367bb011d5e2e4a9e74001d2ee27edfa58c
pauca881 <lopezpaud@gmail.com>
2025-02-05 13:06:45 +0100

    router

--- uno_upc/routes/web.php
+++ uno_upc/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('navbar');
 });
+
+Route::get('/login', function () {
+    return view('navbar');
+});
+
+Route::get('/register', function () {
+    return view('navbar');
+});
+
+
